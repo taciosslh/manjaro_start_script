88814e52f9b7203cb1556aa9905f8dfc279076ee
taciosslh <160537419+taciosslh@users.noreply.github.com>
2025-06-24 14:58:48 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,7 +10,8 @@
 PACKAGES=(
     # Dev
     sqlitebrowser
-    # virtualbox tem que habilitar algumas flag de kernel
+    virtualbox # tem que habilitar algumas flag de kernel
+    dbeaver
 
     # Web
     discord
@@ -20,19 +21,26 @@ PACKAGES=(
     gimp
     inkscape
     audacity
+    darktable
+    obs-studio
 
     # General Tools
     obsidian
     peek
     fd
+    calibre
+    flameshot
+    vlc
+    timeshift
 )
 AUR_PACKAGES=(
     # Dev
-    mongodb-compass
-    mongosh-bin
+    # mongodb-compass
+    # mongosh-bin
     asdf-vm
     visual-studio-code-bin
     # android-studio
+    forticlient-vpn
 
     # General
     zoom
@@ -59,13 +67,13 @@ yay -Sc --noconfirm
 # Install AppImage Software
 
 # Insomnia
-wget https://github.com/Kong/insomnia/releases/download/core%402022.6.0/Insomnia.Core-2022.6.0.AppImage -O Insomnia.AppImage && \
+wget https://github.com/Kong/insomnia/releases/download/core%4011.2.0/Insomnia.Core-11.2.0.AppImage -O Insomnia.AppImage && \
 ail-cli integrate Insomnia.AppImage
 # Beekeper
-wget https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v3.7.10/Beekeeper-Studio-3.7.10.AppImage -O beekeeper.AppImage && \ 
+wget https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v5.2.10/Beekeeper-Studio-5.2.10.AppImage -O beekeeper.AppImage && \ 
 ail-cli integrate beekeeper.AppImage
 # Krita
-wget https://download.kde.org/stable/krita/5.1.3/krita-5.1.3-x86_64.appimage -O krita.AppImage && \ 
+wget https://download.kde.org/stable/krita/5.2.9/krita-5.2.9-x86_64.AppImage -O krita.AppImage && \ 
 ail-cli integrate krita.AppImage
 
 # Configure ZSH
@@ -97,18 +105,19 @@ echo 'source /opt/asdf-vm/asdf.sh' >> ~/.zshrc
 
 # NodeJS
 asdf plugin add nodejs
-asdf install nodejs lts-hydrogen
-asdf global nodejs lts-hydrogen
+asdf install nodejs 20.17.0
+asdf install nodejs 22.16.0
+asdf global nodejs 22.16.0
 
-# python
-asdf plugin add python
-asdf install python 3.11.0
-asdf global python 3.11.0
+# # python
+# asdf plugin add python
+# asdf install python 3.11.0
+# asdf global python 3.11.0
 
 # golang
 asdf plugin add golang
-asdf install golang 1.19.3
-asdf global golang 1.19.3
+asdf install golang 1.24.4
+asdf global golang 1.24.4
 
 
 # TODO replace VSCODE with opensource
